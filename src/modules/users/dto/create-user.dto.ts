89b771db4855a66b9e5400d7f90eb0036333d60d
Tuandev18 <phamtuan180704@gmail.com>
2025-07-13 17:57:36 +0700

    CRUD Users

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+      @IsNotEmpty({message :" Name cannot be blank"})
+      username: string;
+
+      @IsNotEmpty({message : 'Invalid password'})
+      password: string;
+
+      @IsNotEmpty({message: 'please enter full name'})
+      full_name: string;
+
+      @IsNotEmpty({message :" Email cannot be blank"})
+      @IsEmail({}, {message : 'Invalid email'})
+      email: string;
+
+
+      phone: string;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/modules/users/schemas/user.schema';
+import { Model } from 'mongoose';
+import { hashPasswordHelper } from 'src/helpers/util';
 
+import aqp from 'api-query-params';
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) 
+    private userModel: Model<User>,
+    ) {}
+  
+  isEmailExits = async(email :string) =>{
+    const user = await this.userModel.exists({email});
+    if(user) return true;
+    return false;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async create(createUserDto: CreateUserDto) {
+    const {username,password,email,full_name,phone} = createUserDto;
+    //check email 
+    const isExits = await this.isEmailExits(email)
+    if(isExits === true){
+      throw new BadRequestException(`Email đã tồn tại : ${email} . Vui lòng nhập email khác `)
+    }
+    const hashPassword = await hashPasswordHelper(password);
+    const user = await this.userModel.create({
+      username,password : hashPassword ,email,full_name,phone
+    })
+    return {
+      _id : user._id
+    }
+  }
+
+  async findAll(query : string,current : number,pageSize : number) {
+    const {filter,sort} = aqp(query);
+    if (filter.current) delete filter.current;
+    if (filter.pageSize) delete filter.pageSize;
+
+    if(!current) current =1;
+    if(!pageSize) current=10;
+    
+    const totalItems = (await this.userModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+    const skip = (current - 1) * pageSize;
+
+
+    const result = await this.userModel
+    .find(filter)
+    .limit(pageSize)
+    .skip(skip)
+    .sort(sort as any)
+    return {result,totalPages};
   }
 
   findOne(id: number) {
